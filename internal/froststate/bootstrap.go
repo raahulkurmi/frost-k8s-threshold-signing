801b7ef33bc0b1dd7efc972b21473a92d5c1c150
Rahul Chaudhary <raahulchaudhary07@gmail.com>
2026-06-06 16:26:04 +0530

    feat: load shared FROST key material for signers

--- internal/froststate/bootstrap.go
+++ internal/froststate/bootstrap.go
@@ -1,37 +1,72 @@
 package froststate
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"strconv"
-	secretsharing "github.com/bytemare/secret-sharing"
-	"github.com/bytemare/secret-sharing/keys"
-	"github.com/bytemare/ecc"
+
 	"github.com/bytemare/frost"
-	
+	"github.com/bytemare/secret-sharing/keys"
 
 	"frost-k8s-threshold-signing/internal/config"
 )
 
 func Init() error {
 
-	group := ecc.Ristretto255Sha512
-
-	shares, err := secretsharing.Shard(
-		group,
-		nil,
-		3,
-		5,
+	file, err := os.Open(
+		"data/frost-keys.json",
 	)
 
 	if err != nil {
-		return err
+		return fmt.Errorf(
+			"open frost-keys.json: %w",
+			err,
+		)
 	}
 
+	defer file.Close()
+
+	var stored StoredKeys
+
+	if err := json.NewDecoder(file).Decode(
+		&stored,
+	); err != nil {
+		return fmt.Errorf(
+			"decode json: %w",
+			err,
+		)
+	}
+
+	fmt.Println(
+		"Share count:",
+		len(stored.Shares),
+	)
+
+	var keyShares []*keys.KeyShare
 	var publicShares []*keys.PublicKeyShare
 
-	for _, share := range shares {
+	for _, shareHex := range stored.Shares {
+
+		keyShare := &keys.KeyShare{}
+
+		if err := keyShare.DecodeHex(
+			shareHex,
+		); err != nil {
+			return fmt.Errorf(
+				"decode key share: %w",
+				err,
+			)
+		}
+
+		keyShares = append(
+			keyShares,
+			keyShare,
+		)
+
 		publicShares = append(
 			publicShares,
-			share.Public(),
+			keyShare.Public(),
 		)
 	}
 
@@ -39,39 +74,55 @@ func Init() error {
 		Ciphersuite:           frost.Default,
 		Threshold:             3,
 		MaxSigners:            5,
-		VerificationKey:       shares[0].VerificationKey,
+		VerificationKey:       keyShares[0].VerificationKey,
 		SignerPublicKeyShares: publicShares,
 	}
 
 	if err := frostConfig.Init(); err != nil {
-		return err
+		return fmt.Errorf(
+			"init config: %w",
+			err,
+		)
 	}
 
-
 	signerID, err := strconv.Atoi(
-	config.SignerID(),
-)
-
-if err != nil {
-	return err
-}
-
-shareIndex := signerID - 1
-
-	signer, err := frostConfig.Signer(
-		shares[shareIndex],
+		config.SignerID(),
 	)
 
 	if err != nil {
-		return err
+		return fmt.Errorf(
+			"parse signer id: %w",
+			err,
+		)
 	}
 
-	
-	println(
-	"Loaded signer",
-	signer.Identifier(),
-)
-    Signer = signer
+	shareIndex := signerID - 1
+
+	if shareIndex < 0 || shareIndex >= len(keyShares) {
+		return fmt.Errorf(
+			"invalid signer id %d",
+			signerID,
+		)
+	}
+
+	signer, err := frostConfig.Signer(
+		keyShares[shareIndex],
+	)
+
+	if err != nil {
+		return fmt.Errorf(
+			"create signer: %w",
+			err,
+		)
+	}
+
+	fmt.Println(
+		"Loaded signer",
+		signer.Identifier(),
+	)
+
+	Signer = signer
 	Config = frostConfig
+
 	return nil
 }
